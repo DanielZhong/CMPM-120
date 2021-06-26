2e601980ca31dc9203c238c8fec0ba82161e3d49
unknown <danielzhong2000@gmail.com>
2021-06-26 16:36:09 -0700

    update

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -3,12 +3,17 @@ class Play extends Phaser.Scene {
         super("playScene");
     }
 
+
+    preload(){
+        this.load.image('rocket', '.assets/rocket.png');
+        this.load.image('spaceship', '.assets/spaceship.png');
+        this.load.image('starfield', '.assets/starfield.png');
+    }
+
     create(){
         this.add.text(20, 20, "Rocket Patrol play");
+
         
-        //green ui background
-        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0,0);
-        this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0)
     }
 
 }
