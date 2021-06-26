f79e68818af61f90c9f382487db6d648b3a107b4
unknown <danielzhong2000@gmail.com>
2021-06-26 03:08:36 -0700

    update

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -5,6 +5,8 @@ class Play extends Phaser.Scene {
 
     create(){
         this.add.text(20, 20, "Rocket Patrol play");
+        //green ui background
+        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0,0);
     }
 
 }
